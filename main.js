d4663ab11984a3f8653485ef26cc713ee472fe8c
vitalii <dumavb777@gmail.com>
2019-05-22 19:38:19 +0300

    develop2

--- main.js
+++ main.js
@@ -1,27 +1,11 @@
 const tree = document.getElementById('tree');
 
-function hideElementChildren(event) {
-    const htmlTeg = event.target;
-    if (htmlTeg.nodeName === 'SPAN' && htmlTeg.nextElementSibling !== null) {
-        htmlTeg.nextElementSibling.classList.toggle('hide');
+function hideElementSibling(event) {
+    const {target: tagName} = event;
+
+    if (tagName.nodeName === 'SPAN' && tagName.nextElementSibling !== null) {
+        tagName.nextElementSibling.classList.toggle('hide');
     }
 };
 
-function textBold(event) {
-    const htmlTeg = event.target;
-    if (htmlTeg.nodeName === 'SPAN') {
-        htmlTeg.classList.add('bold');
-    }
-
-}
-
-function textNorm(event) {
-    const htmlTeg = event.target;
-    if (htmlTeg.nodeName === 'SPAN') {
-        htmlTeg.classList.remove('bold');
-    }
-}
-
-tree.addEventListener('click', hideElementChildren);
-tree.addEventListener('mouseover', textBold);
-tree.addEventListener('mouseout', textNorm);
+tree.addEventListener('click', hideElementSibling);
